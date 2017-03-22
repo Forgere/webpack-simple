6ee6869f3d189c3992cd584749c10492024553d8
forgere <573596314@qq.com>
2017-03-22 17:48:57 +0800

    simple

--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+{{#router}}
+import router from './router'
+{{/router}}
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  {{#router}}
+  router,
+  {{/router}}
+})
+new Vue({
+  el: '#app',
+  {{#router}}
+  router,
+  {{/router}}
+  template: '<App/>',
+  components: { App }
 })
