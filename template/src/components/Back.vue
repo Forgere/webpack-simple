34a0b23d2f02710dc4c19416de58621b2df04054
forgere <573596314@qq.com>
2017-03-23 12:24:03 +0800

    router样式实例

---
+++ template/src/components/Back.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="back">
+    <router-link to='/'>返回</router-link>
+    <router-link to='/back/details'>详情</router-link>
+    <div>123</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'back',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+body{
+  transform: translate(0,0);
+  h1, h2 {
+    font-weight: normal;
+  }
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '../components/Hello.vue'
-
+import Back from '../components/Back.vue'
+import BackDetail from '../components/BackDetail.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/back',
+      name: 'Back',
+      component: Back,
+      children: [{
+        path: 'details',
+        name: 'details',
+        component: BackDetail
+      }]
     }
   ]
 })
